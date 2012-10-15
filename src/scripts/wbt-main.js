7333870613f1d67a509f81859ab0c17e71c22342
Robert Sanders <dotperson@gmail.com>
2012-10-14 20:45:35 -0400

    Switching from GPL to MPL v2.0, I used GPL because I was in a hurry and I think MPL is closer to what I want, which is to allow reuse as long as changes to the core are contributed back.

--- src/scripts/wbt-main.js
+++ src/scripts/wbt-main.js
@@ -1,20 +1,10 @@
-/* 
-Copyright (C) 2011 iPOV.net
+/*
+Copyright (C) 2011-2012 iPOV.net
 Author: Robert Sanders (dotperson@gmail.com)
 
 This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
+modify it under the terms of the Mozilla Public License v2 or higher.
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 require.config({
     locale: net_ipov_cfg.locale     // .locale is determined and set in _init.js which is run BEFORE require.js is loaded
